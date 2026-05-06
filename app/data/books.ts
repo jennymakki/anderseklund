820f3f53b21128c8562a253d1d772255c7bcf3ec
jennymakki <jjennyeklundd@hotmail.com>
2026-05-06 10:25:43 +0200

    added more book info

--- app/data/books.ts
+++ app/data/books.ts
@@ -3,14 +3,14 @@ export const books = [
     id: "1",
     title: "Tillbakablick Heby - En by i förändring",
     description: "Beskriv bokens innehåll här. Vad handlar den om, vilken tid, vilka människor eller platser?",
-    image: "/books/placeholder.jpg",
+    image: "/books/tillbakablick.png",
     price: "250 kr",
   },
   {
     id: "2",
     title: "Club Shake",
     description: "Ett minne från en tid när Heby var navet för tonåringarna i städer och byar kring Heby. Ett minne av musiken, banden, hysterin och upplevelsen, som dagens 75 åringar fick uppleva i Heby när de var tonåringar.",
-    image: "/books/placeholder.jpg",
+    image: "/books/clubshake.png",
     price: "250 kr",
   },
   {

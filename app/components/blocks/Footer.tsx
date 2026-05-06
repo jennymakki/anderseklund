5ca74828dd4e5b154ec1cc181f0304fe97ae9e35
jennymakki <jjennyeklundd@hotmail.com>
2026-05-06 12:33:05 +0200

    trying to fix safari footer bug

--- app/components/blocks/Footer.tsx
+++ app/components/blocks/Footer.tsx
@@ -1,16 +1,14 @@
 export default function Footer() {
   return (
     <footer className="relative bg-[#D8DBDF] w-full pt-32">
-
-      <div className="absolute  left-1/2 -translate-x-1/2 -top-24">
+      <div className="px-6 md:px-20 bg-[#122D48] py-10 text-center">
+              <div className="absolute  left-1/2 -translate-x-1/2 -top-24">
         <img 
           src="/siluett.png"
           alt="Heby Historia"
           className="pointer-events-none h-40 md:h-56 w-auto block"
         />
       </div>
-
-      <div className="px-6 md:px-20 bg-[#122D48] py-10 text-center">
         <p className="text-[var(--color-beige)] text-xs md:text-sm">
           © {new Date().getFullYear()} Heby Historia – Lokala berättelser från hjärtat av Heby
         </p>

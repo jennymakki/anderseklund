b3ea8d935a15fc703273d39b0b908e2b1e28e272
jennymakki <jjennyeklundd@hotmail.com>
2026-07-08 19:19:41 +0200

    updated event

--- app/data/events.ts
+++ app/data/events.ts
@@ -1,15 +1,24 @@
 export const events = [
-  {
+    {
     id: 1,
+    title: "Författarträff",
+    date: "2026-08-12",
+    displayDate: "12 augusti 2026",
+    time: "18:30",
+    description:
+      "Kom och träffa mig på Jessans Café i Heby för att diskutera Hebys historia och mina böcker. Fredrik Eklund kommer dessutom spela live-musik.",
+  },
+  {
+    id: 2,
     title: "Bokmässa i Sala",
     date: "2026-08-30",
     displayDate: "28-30 augusti 2026",
-    time: "Återkommer om tid",
+    time: "Återkommer om exakt tid",
     description:
       "Mer info kommer",
   },
   {
-    id: 2,
+    id: 3,
     title: "Minnesinsamling om Heby",
     date: null,
     displayDate: "Pågående insamling",
